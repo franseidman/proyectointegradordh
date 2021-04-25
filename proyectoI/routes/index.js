e330fadae21e50f964959feac5f5e70ef2bf7bc5
franseidman <fseidman@udesa.edu.ar>
2021-04-25 20:12:22 -0300

    limpieza

--- proyectoI/routes/index.js
+++ proyectoI/routes/index.js
@@ -2,19 +2,9 @@ var express = require('express');
 var router = express.Router();
 const mainController = require('../controllers/mainController')
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-
 router.get('/', mainController.index); 
 router.get('/register', mainController.register);
 router.get('/login', mainController.login);
 router.get('/search-results', mainController.searchResults);
-//router.get('/product/', mainController.product);
-//router.get('/product-add', mainController.productAdd);
-//router.get('/profile', mainController.profile);
-//router.get('/profile-edit', mainController.profileEdit);
 
 module.exports = router;
--- proyectoI/routes/product.js
+++ proyectoI/routes/product.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController')
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-
 router.get('/', productController.product);
 router.get('/add', productController.productAdd);
 router.get('/edit', productController.productEdit);
