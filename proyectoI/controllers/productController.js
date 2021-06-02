b699b2c6ad78930a4cec0412cca0ef81c46516f0
mpautassocav <mpautassocavanagh@udesa.edu.ar>
2021-06-02 13:10:32 -0300

    Avance: Buscador funcional, problemas con forms

--- proyectoI/controllers/productController.js
+++ proyectoI/controllers/productController.js
@@ -1,3 +1,7 @@
+const db = require('../database/models'); //Requerimos la conexión a la base de datos y todos los modelos.
+const op = db.Sequelize.Op
+
+
 const productController = {
     product: function(req, res){
         return res.render('product')
@@ -7,6 +11,53 @@ const productController = {
     },
     productEdit: function(req, res){
         return res.render('product-edit')
+    },
+    create: function(req, res){
+        //Mostrar formulario de carga de películas
+        db.Product.findAll()
+            .then( data => {
+                return res.render('movieNew', {genres:data});
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },
+    store: function(req, res){
+        //Método para guardar nueva película.
+        //1) Obtener datos del formulario
+        let data = req.body;
+        
+        //2)Crear pelicula nueva.
+        let product = {           
+            imagen: data.imagen,
+            nombre: data.nombre,
+            creacion: data.creacion,
+            descripcion: data.descripcion,
+        }
+        //3)Guardar película
+        db.Product.create(product)
+            .then( (nuevoProducto) => {
+        //4)Redirección
+                return res.redirect('/');
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },
+    destroy: function(req, res){
+        let movieABorrar = req.params.id;
+        
+        db.Movie.destroy({
+            where: [
+                {id : movieABorrar}
+            ]
+        })
+            .then( () => {
+                 return res.redirect('/');
+            })
+            .catch( error => { 
+                console.log(error);
+            })
     }
 }
 
--- proyectoI/routes/product.js
+++ proyectoI/routes/product.js
@@ -5,5 +5,9 @@ const productController = require('../controllers/productController')
 router.get('/', productController.product);
 router.get('/add', productController.productAdd);
 router.get('/edit', productController.productEdit);
+router.get('/create', productController.create);
+router.post('/store', productController.store);
+router.post('/delete/:id', productController.destroy);
+
 
 module.exports = router;
